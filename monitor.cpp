cf8b4445a30a03fdddc0f90437562bb05703125d
Teklad <jkinnaird1991@gmail.com>
2018-05-16 20:27:59 -0500

    Simplify integration code.

--- monitor.cpp
+++ monitor.cpp
@@ -12,7 +12,7 @@
 static std::atomic<bool> m_csgoActive;
 
 static bool isFeatureSupported(Display *dpy, const char *feature)
-{   
+{
     Atom nameAtom = XInternAtom(dpy, "_NET_SUPPORTED", True);
     Atom featureAtom = XInternAtom(dpy, feature, True);
     Atom type;
@@ -33,68 +33,23 @@ static bool isFeatureSupported(Display *dpy, const char *feature)
                 return true;
             }
         }
-    } else if (data != 0) {
         XFree(data);
     }
     return false;
-}
-
-static int getActiveWindow(Display *dpy, Window *window_ret)
-{   
-    Atom nameAtom = XInternAtom(dpy, "_NET_ACTIVE_WINDOW", True);
-    Atom type;
-    Window root;
-    int format, status;
-    unsigned long nitems, after;
-    unsigned char *data;
-    
-    root = XDefaultRootWindow(dpy);
-    status = XGetWindowProperty(dpy, root, nameAtom, 0, 1024, 0,
-            XA_WINDOW, &type, &format, &nitems, &after, &data);
-    
-    if (status == Success && data != 0) {
-        *window_ret = *((Window*)data);
-        XFree(data);
-    } else {
-        *window_ret = 0;
-    }
-    
-    return nitems > 0 ? 1 : 0;
-}
-
-static long getWindowPID(Display *dpy, Window window)
-{
-    Atom nameAtom = XInternAtom(dpy, "_NET_WM_PID", True);
-    Atom type;
-    int format, status;
-    long pid = -1;
-    unsigned long nitems, after;
-    unsigned char *data;
-
-    status = XGetWindowProperty(dpy, window, nameAtom, 0, 1024, 0,
-            XA_CARDINAL, &type, &format, &nitems, &after, &data);
-    if (status == Success && data != 0) {
-        pid = *((long*)data);
-        XFree(data);
-    }
-    return pid;
 }
 
 static Display *initializeDisplay()
 {
     Display *dpy = XOpenDisplay(NULL);
     if (dpy != NULL) {
-        if (isFeatureSupported(dpy, "_NET_WM_PID") &&
-                isFeatureSupported(dpy, "_NET_ACTIVE_WINDOW")) {
+        if (isFeatureSupported(dpy, "_NET_WM_PID")) {
             XSelectInput(dpy, XDefaultRootWindow(dpy), SubstructureNotifyMask);
-            puts("_NET_WM_PID and _NET_ACTIVE_WINDOW supported.  Enabling window manager integration.");
-            return dpy;
+            puts("_NET_WM_PID supported.  Enabling window manager integration.");
         } else {
             XCloseDisplay(dpy);
         }
     }
-
-    return NULL;
+    return dpy;
 }
 
 static void closeDisplay(Display *dpy)
@@ -105,36 +60,41 @@ static void closeDisplay(Display *dpy)
     }
 }
 
-/**
- * @brief Checks whether CSGO is being actively used.
- *
- * @param dpy
- *
- * @return bool
- */
 static bool checkCSGOActive(Display *dpy)
 {
-    Window activeWindow;
-    int pid = -1;
-    ssize_t rlsize;
-    char pathbuf[512];
-    char outbuf[512];
+    Window window;
+    int revert;
+    Atom propAtom = XInternAtom(dpy, "_NET_WM_PID", True);
+    Atom type;
+    int format, status;
+    long pid = -1;
+    unsigned long nitems, after;
+    unsigned char* data;
 
-    if (getActiveWindow(dpy, &activeWindow)) {
-        pid = getWindowPID(dpy, activeWindow);
+    XGetInputFocus(dpy, &window, &revert);
+
+    status = XGetWindowProperty(dpy, window, propAtom, 0, 1024, 0,
+            XA_CARDINAL, &type, &format, &nitems, &after, &data);
+    if (status == Success && data != 0) {
+        pid = *((long*)data);
+        XFree(data);
     }
-
+    
     if (pid == -1) {
         return false;
     }
-
-    snprintf(pathbuf, 512, "/proc/%i/exe", pid);
-    rlsize = readlink(pathbuf, outbuf, sizeof(pathbuf)-1);
+    
+    char pathbuf[512];
+    char rlbuf[512];
+    ssize_t rlsize;
+    
+    snprintf(pathbuf, 512, "/proc/%li/exe", pid);
+    rlsize = readlink(pathbuf, rlbuf, sizeof(pathbuf)-1);
     if (rlsize == -1) {
         return false;
     }
-    outbuf[rlsize] = '\0';
-    return strstr(outbuf, "csgo_linux64") != NULL;
+    rlbuf[rlsize] = '\0';
+    return strstr(rlbuf, "csgo_linux64") != NULL;
 }
 
 bool isCSGOActive()
@@ -145,14 +105,11 @@ bool isCSGOActive()
 void csgoActivityLoop()
 {
     Display *dpy = initializeDisplay();
-
     XEvent ev;
     while (dpy) {
         XNextEvent(dpy, &ev);
-        switch (ev.type) {
-            case ConfigureNotify:
-                m_csgoActive = checkCSGOActive(dpy);
-                break;
+        if (ev.type == ConfigureNotify) {
+            m_csgoActive = checkCSGOActive(dpy);
         }
     }
     m_csgoActive = true;
